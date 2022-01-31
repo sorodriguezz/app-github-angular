65111e64b07cb449e65969c9c2a4ca14ebc6345c
soRodriguezz <s.rodriguezzapata01@gmail.com>
2022-01-30 22:20:58 -0300

    perfil

--- src/app/components/perfil/perfil.component.ts
+++ src/app/components/perfil/perfil.component.ts
@@ -1,24 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { GithubService } from '../../services/github.service';
-import {Perfil} from "../../interfaces/perfil.interface";
+import { Perfil } from '../../interfaces/perfil.interface';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
-  styleUrls: ['./perfil.component.css']
+  styleUrls: ['./perfil.component.css'],
 })
 export class PerfilComponent implements OnInit {
-
   public data!: Perfil;
+  public propiedadModal: any;
 
-  constructor(
-    private githubService: GithubService
-  ) { }
+  constructor(private githubService: GithubService) {}
 
   ngOnInit(): void {
-    this.githubService.obtenerPerfilGithub().subscribe( resp => {
+    this.obtenerPerfilGithub();
+  }
+
+  private obtenerPerfilGithub() {
+    this.githubService.obtenerPerfilGithub().subscribe((resp) => {
       this.data = resp;
     });
   }
 
+  public abrirModal(tipo: any) {
+    this.propiedadModal = 'block';
+  }
+
+  public cambioEstado(estado: any) {
+    this.propiedadModal = estado;
+  }
 }
